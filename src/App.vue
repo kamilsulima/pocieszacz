5e824782c5fc31defc30c7676a263f1552147b35
Kamil Sulima <sulima.kamil@gmail.com>
2018-03-20 11:52:58 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+   <nav>
+     <router-link to="/">Start</router-link>
+     <router-link to="/about">O projekcie</router-link>
+   </nav>
+
+     <router-view />
+  </div>
+
+
+</template>
+
+<script>
+import Solace from './components/Solace.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Solace
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  padding-top: 50px;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
+#app {
+    width: 50%;
+}
+
+nav {
+  padding: 20px 20px 20px 0;
+}
+
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: bold;
+  margin-right: 15px;
+}
+</style>
---
+++ src/components/About.vue
@@ -0,0 +1,6 @@
+<template>
+   <div class="about">
+    <h1>Możesz wszytko...</h1>
+    <p>Pocieszający będą pocieszeni.</p>
+  </div>
+</template>
